3520d3418dee64003a6c9c81218683d3f9985936
Skallfa <128469350+Skallfa@users.noreply.github.com>
2024-01-17 13:25:47 +0530

    Update QuicksettingsSettings.java

--- src/com/android/settings/quicksettings/QuicksettingsSettings.java
+++ src/com/android/settings/quicksettings/QuicksettingsSettings.java
@@ -6,44 +6,69 @@
 
 package com.android.settings.quicksettings;
 
-import android.content.ContentResolver;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import static android.os.UserHandle.USER_CURRENT;
+import static android.os.UserHandle.USER_SYSTEM;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
 import android.content.Context;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.content.om.IOverlayManager;
+import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.provider.Settings;
-
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import androidx.preference.ListPreference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.View;
 
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.settings.R;
-import com.android.settings.dashboard.DashboardFragment;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.SearchIndexable;
+import com.android.settings.custom.preference.SystemSettingListPreference;
 
-import java.util.Arrays;
+import java.util.Locale;
 import java.util.List;
+import java.util.ArrayList;
 
 @SearchIndexable
-public class QuicksettingsSettings extends DashboardFragment implements OnPreferenceChangeListener {
-
-    private static final String TAG = "QuicksettingsSettings";
-    private static final String KEY_QS_PANEL_STYLE  = "qs_panel_style";
+public class QuicksettingsSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+    
+    private static final String KEY_QS_PANEL_STYLE = "qs_panel_style";
 
     private Handler mHandler;
-    private ListPreference mQsStyle;
+    private IOverlayManager mOverlayManager;
+    private IOverlayManager mOverlayService;
+    private SystemSettingListPreference mQsStyle;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.quicksettings_settings);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
 
-        mQsStyle = (ListPreference) findPreference(KEY_QS_PANEL_STYLE);
+        mOverlayService = IOverlayManager.Stub
+        .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+
+        mQsStyle = (SystemSettingListPreference) findPreference(KEY_QS_PANEL_STYLE);
         mCustomSettingsObserver.observe();
+
     }
 
     private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
@@ -69,24 +94,20 @@ public class QuicksettingsSettings extends DashboardFragment implements OnPrefer
         }
     }
 
-
-    @Override
-    public int getMetricsCategory() {
-        return MetricsEvent.CUSTOM_SETTINGS;
-    }
-
-    @Override
-    protected String getLogTag() {
-        return TAG;
-    }
-
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mQsStyle) {
-           mCustomSettingsObserver.observe();
-           return true;
-        }
+               mCustomSettingsObserver.observe();
+            return true;
+          } 
         return false;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_PANEL_STYLE, 0, UserHandle.USER_CURRENT);
     }
 
     private void updateQsStyle() {
@@ -95,33 +116,68 @@ public class QuicksettingsSettings extends DashboardFragment implements OnPrefer
         int qsPanelStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.QS_PANEL_STYLE , 0, UserHandle.USER_CURRENT);
 
-        switch (qsPanelStyle) {
-            case 0:
-              setQsStyle("com.android.systemui");
-              break;
-            case 1:
-              setQsStyle("com.android.system.qs.outline");
-              break;
-            case 2:
-              setQsStyle("com.android.system.qs.twotoneaccent");
-              break;
-            case 3:
-              setQsStyle("com.android.system.qs.shaded");
-              break;
-            default:
-              break;
+        if (qsPanelStyle == 0) {
+            setDefaultStyle(mOverlayService);
+        } else if (qsPanelStyle == 1) {
+            setQsStyle(mOverlayService, "com.android.system.qs.outline");
+        } else if (qsPanelStyle == 2 || qsPanelStyle == 3) {
+            setQsStyle(mOverlayService, "com.android.system.qs.twotoneaccent");
+        } else if (qsPanelStyle == 4) {
+            setQsStyle(mOverlayService, "com.android.system.qs.shaded");
+        } else if (qsPanelStyle == 5) {
+            setQsStyle(mOverlayService, "com.android.system.qs.cyberpunk");
+        } else if (qsPanelStyle == 6) {
+            setQsStyle(mOverlayService, "com.android.system.qs.neumorph");
+        } else if (qsPanelStyle == 7) {
+            setQsStyle(mOverlayService, "com.android.system.qs.reflected");
+        } else if (qsPanelStyle == 8) {
+            setQsStyle(mOverlayService, "com.android.system.qs.surround");
+        } else if (qsPanelStyle == 9) {
+            setQsStyle(mOverlayService, "com.android.system.qs.thin");
+        }    
+    }
+
+    public static void setDefaultStyle(IOverlayManager overlayManager) {
+        for (int i = 0; i < QS_STYLES.length; i++) {
+            String qsStyles = QS_STYLES[i];
+            try {
+                overlayManager.setEnabled(qsStyles, false, USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    @Override
-    protected int getPreferenceScreenResId() {
-        return R.xml.quicksettings_settings;
+    public static void setQsStyle(IOverlayManager overlayManager, String overlayName) {
+        try {
+            for (int i = 0; i < QS_STYLES.length; i++) {
+                String qsStyles = QS_STYLES[i];
+                try {
+                    overlayManager.setEnabled(qsStyles, false, USER_SYSTEM);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+            overlayManager.setEnabled(overlayName, true, USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
+    public static final String[] QS_STYLES = {
+        "com.android.system.qs.outline",
+        "com.android.system.qs.twotoneaccent",
+        "com.android.system.qs.shaded",
+        "com.android.system.qs.cyberpunk",
+        "com.android.system.qs.neumorph",
+        "com.android.system.qs.reflected",
+        "com.android.system.qs.surround",
+        "com.android.system.qs.thin"
+    };
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.quicksettings_settings);
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_SETTINGS;
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
